334fb18fd05e9f9705043b93c8c3943fd6f0cabd
sun-23 <twinsamson@gmail.com>
2021-02-10 15:41:25 +0700

    add sendcommand and disconnect button

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ const SEND_SERVICE_CHARACTERISTIC = 0xFFE1;
 
 function App() {
 
-  const [character, set_characteristic] = useState();
-  const [device_name, set_device_name ] = useState();
+  const [ character, set_characteristic] = useState();
+  const [ device , setDevice ] = useState();
+  const [ command, setCommand ] = useState();
 
   const request_device = async () => {
     const device = await navigator.bluetooth
@@ -17,7 +18,17 @@ function App() {
    const service = await server.getPrimaryService(SEND_SERVICE);
    const characteristic = await service.getCharacteristic(SEND_SERVICE_CHARACTERISTIC);
    set_characteristic(characteristic);
-   set_device_name(device.name);
+   setDevice(device);
+  }
+
+  async function disconnect(){
+    if(device){
+      await device.gatt.disconnect();
+      setDevice();
+      set_characteristic();
+    }else{
+      alert('no device connected.');
+    }
   }
 
   function on(event){
@@ -31,13 +42,34 @@ function App() {
     console.log(code);
     character.writeValue(Uint8Array.of(code));
   }
+  
+  function send_command(e){
+    e.preventDefault();
+    let enc = new TextEncoder(); // By default this encodes to utf-8
+    // Why the <opening and closing> characters?
+    // Went with this guy's example 3 for the reasons he mentions: https://forum.arduino.cc/index.php?topic=396450.0
+    character.writeValue(enc.encode(`${command}`));
+    setCommand('');
+  }
 
   return (
     <div>
-      <h1>device: { device_name }</h1>
+      <h1>device: { device ? device.name : 'nodevice' }</h1>
       <button onClick={() => request_device()}>connect</button>
+      <button onClick={() => disconnect()}>disconnect</button>
       <button onClick={on} data-code="1">on</button>
       <button onClick={off} data-code="0">off</button>
+      <form onSubmit={send_command}>
+        <label htmlFor='firstName'>Name : </label>
+            <input
+              type='text'
+              id='firstName'
+              name='command'
+              value={command}
+              onChange={(e) => setCommand(e.target.value)}
+            />
+            <button type='submit'>send command</button>
+      </form>
     </div>
   );
 }
